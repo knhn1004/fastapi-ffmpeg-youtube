317c8438137d13c4131dde0c3730ba3cc0596f44
knhn1004 <49494541+knhn1004@users.noreply.github.com>
2024-06-07 15:12:21 -0700

    remove flip coin endpoints

--- server.py
+++ server.py
@@ -12,38 +12,6 @@ from pytube import YouTube
 app = FastAPI()
 
 
-@app.get("/")
-def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/flip-coin")
-def flip_coin():
-    val = random.random()
-    head_tail = ""
-    if (val <= 0.5):
-        head_tail = "heads"
-    else:
-        head_tail = "tails"
-    return ({"value": head_tail})
-
-
-@app.get("/flip-coins")
-def flip_coins(times: int):
-    if times and times > 0:
-        head_count = 0
-        tail_count = 0
-        for _ in range(times):
-            val = random.random()
-            if (val <= 0.5):
-                head_count += 1
-            else:
-                tail_count += 1
-        return ({"heads": head_count, "tails": tail_count})
-    else:
-        return ({"message": "you need to send valid times"})
-
-
 class PlayRequest(BaseModel):
     url: str
 
