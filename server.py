715b5d60a3ad2e03801d8a4c482b0296c4d01891
knhn1004 <49494541+knhn1004@users.noreply.github.com>
2024-06-07 19:11:22 -0700

    replaced asyncio with threading

--- server.py
+++ server.py
@@ -1,10 +1,10 @@
 import uvicorn
-import random
 import uuid
 import os
-import asyncio
+import subprocess
+import threading
 
-from fastapi import FastAPI, BackgroundTasks
+from fastapi import FastAPI
 from fastapi.exceptions import HTTPException
 from pydantic import BaseModel
 from pytube import YouTube
@@ -20,7 +20,7 @@ SAVE_PATH = "downloads"
 STREAM_URL = "rtmp://localhost:1935/live/mystream"
 
 
-async def download_and_stream_video(video_url: str):
+def download_and_stream_video(video_url: str):
     try:
         yt = YouTube(video_url)
     except:
@@ -33,25 +33,26 @@ async def download_and_stream_video(video_url: str):
             break
     try:
         # Downloading the video
-        await asyncio.to_thread(stream.download, SAVE_PATH, filename=filename)
+        stream.download(SAVE_PATH, filename=filename)
     except Exception as e:
         raise HTTPException(status_code=500, detail="Unable to download video")
 
     command = ["ffmpeg", "-re", "-i", f"./downloads/{filename}",
                "-c:v", "libx264", "-preset", "veryfast", "-tune",
-               "zerolatency", "-c:a", "aac", "-ar", "44100", "-f", "flv",
-               STREAM_URL]
-    process = await asyncio.create_subprocess_exec(*command, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
-    await process.wait()
+                       "zerolatency", "-c:a", "aac", "-ar", "44100", "-f", "flv",
+                       STREAM_URL]
+    subprocess.Popen(command, stdout=subprocess.DEVNULL,
+                     stderr=subprocess.DEVNULL)
 
 
 @app.post("/play")
-async def play(play_request: PlayRequest, background_tasks: BackgroundTasks):
-    background_tasks.add_task(
-        asyncio.run, download_and_stream_video(play_request.url))
+def play(play_request: PlayRequest):
+    t = threading.Thread(target=download_and_stream_video,
+                         args=(play_request.url,))
+    t.start()
 
     return ({"message": "streaming video soon: rtmp://localhost:1935/live/mystream", })
 
 
 if __name__ == "__main__":
-    uvicorn.run("server:app", port=5000, reload=True)
+    uvicorn.run("server:app", port=8080, reload=True)
